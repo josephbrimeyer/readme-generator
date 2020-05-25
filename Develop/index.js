d2b409ef7aaf76fc77c27a0d810b268861c4003c
Joe Brimeyer <jbrimeyer@gmail.com>
2020-05-25 16:15:09 -0700

    added list of questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,28 +1,43 @@
 // need to install inquirer npm and learn how to use it to get the readme data.
 // examples 13, 14, and 15 have sample code on how to write to a json file, here we will write to an md file.
 
-var inquirer = require("inquirer");
-var fs = require('fs');
+let inquirer = require("inquirer");
+let fs = require('fs');
 
 inquirer.prompt([
   {
     type: "input",
-    name: "name",
-    message: "What is your name?"
+    name: "title",
+    message: "What is the title of your project?"
   },
   {
-    type: "checkbox",
-    message: "What languages do you know?",
-    name: "stack",
-    choices: [
-      "HTML", 
-      "CSS", 
-      "JavaScript", 
-      "MySQL"
-    ]
+    type: "input",
+    name: "description",
+    message: "Enter a description for this project."
   },
   {
     type: "list",
+    message: "What license would you like to use for this project?",
+    name: "license",
+    choices: [
+      "MIT",
+      "Apache",
+      "GNU",
+      "Mozilla"
+    ]
+  },
+  {
+    type: "input",
+    name: "GitHubUserId",
+    message: "Enter your GitHub user id."
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Describe the installation of this app."
+  },
+  {
+    type: "checkbox",
     message: "What is your preferred method of communication?",
     name: "contact",
     choices: [
@@ -30,18 +45,72 @@ inquirer.prompt([
       "phone",
       "telekinesis"
     ]
+
   }
-]).then(function(data) {
+]).then(function (data) {
 
-  var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+  function writeToFile(fileName, data) {
+    let filename = data.name.toLowerCase().split(' ').join('') + ".json";
 
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
 
-    if (err) {
-      return console.log(err);
-    }
+      if (err) {
+        return console.log(err);
+      }
 
-    console.log("Success!");
+      console.log("Success!");
 
-  });
+    });
+  };
 });
+
+function init() {
+
+}
+
+init();
+
+// var inquirer = require("inquirer");
+// var fs = require('fs');
+
+// inquirer.prompt([
+//   {
+//     type: "input",
+//     name: "name",
+//     message: "What is your name?"
+//   },
+//   {
+//     type: "checkbox",
+//     message: "What languages do you know?",
+//     name: "stack",
+//     choices: [
+//       "HTML", 
+//       "CSS", 
+//       "JavaScript", 
+//       "MySQL"
+//     ]
+//   },
+//   {
+//     type: "list",
+//     message: "What is your preferred method of communication?",
+//     name: "contact",
+//     choices: [
+//       "email",
+//       "phone",
+//       "telekinesis"
+//     ]
+//   }
+// ]).then(function(data) {
+
+//   var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+
+//   fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+//     if (err) {
+//       return console.log(err);
+//     }
+
+//     console.log("Success!");
+
+//   });
+// });
